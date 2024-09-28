eaf8d429c9428930cc7965ae4a61a8e189d4d62c
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-09-28 10:00:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.scss";
 import "swiper/scss";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
+
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
